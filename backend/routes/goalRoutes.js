8109333990e053c73f8da65f6d2ee07f0f6dfd9f
Brian Otieno <brianotieno586@gmail.com>
2024-02-27 12:41:25 +0300

    Goals controller and routes setup

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
+const router = express.Router();
+
+router.route("/").get(getGoals).post(setGoal);
+router.route("/:id").delete(deleteGoal).put(updateGoal);
+
+module.exports = router;
